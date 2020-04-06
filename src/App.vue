4a8af3f112459f5362939aee53f34344255b212f
kete <ketenet@163.com>
2020-04-06 11:57:02 +0800

    add a header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <!-- <img src="./assets/logo.png"> -->
+    <Header></Header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Header from './components/Header'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px;*/
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="header">
+		<span>Tode-list</span>
+	</div>
+</template>
+
+<script>
+	
+</script>
+
+<style>
+	.header {
+		width: 100%;
+		height: 100px;
+		background-color: #03031B;
+		text-align: center;
+		color: #fff;
+		line-height: 100px;
+		font-size: 28px;
+		font-weight: bold;
+	}
+</style>
